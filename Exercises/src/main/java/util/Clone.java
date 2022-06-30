ea138ab75d163b0bf7b9cb500b83eae959a15ce0
Pedro Santos <pedrojjsantos.cc@gmail.com>
2022-06-30 14:33:15 -0300

    Exercise 12: Q3 (INCOMPLETE) Skipped Q1.

--- Exercises/src/main/java/util/Clone.java
+++ Exercises/src/main/java/util/Clone.java
@@ -1,11 +1,12 @@
 package util;
 
 import java.io.*;
+import java.lang.reflect.*;
 
 public class Clone {
     private Clone() {}
 
-    public static Object clone(Object obj) throws IOException, ClassNotFoundException {
+    public static Object cloneSerialized(Object obj) throws IOException, ClassNotFoundException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         ObjectOutputStream outputStream = new ObjectOutputStream(byteStream);
         outputStream.writeObject(obj);
@@ -13,5 +14,18 @@ public class Clone {
         ObjectInputStream inputStream = new ObjectInputStream(new ByteArrayInputStream(array));
 
         return inputStream.readObject();
+    }
+
+    public static Object cloneReflection(Object obj) throws Exception {
+        Field[] fields = obj.getClass().getDeclaredFields();
+        Object copy = obj.getClass().getConstructor().newInstance();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Field copyField = copy.getClass().getDeclaredField(field.getName());
+            copyField.set(copy, field.get(obj));
+        }
+
+        return copy;
     }
 }
--- Exercises/src/test/java/util/CloneTest.java
+++ Exercises/src/test/java/util/CloneTest.java
@@ -10,16 +10,32 @@ import static org.junit.Assert.*;
 
 public class CloneTest {
     @Test
-    public void testClone() throws IOException, ClassNotFoundException {
+    public void testCloneSerialized() throws IOException, ClassNotFoundException {
         String str = "123456789";
-        String strCopy = (String) Clone.clone(str);
+        String strCopy = (String) Clone.cloneSerialized(str);
 
         assertEquals(str, strCopy);
         assertNotSame(str, strCopy);
 
         Board board = new Board();
         board.put("a6", Piece.createBlackRook());
-        Board boardCopy = (Board) Clone.clone(board);
+        Board boardCopy = (Board) Clone.cloneSerialized(board);
+
+        assertEquals(board.print(), boardCopy.print());
+        assertNotSame(board, boardCopy);
+    }
+
+    @Test
+    public void testCloneReflection() throws Exception {
+        String str = "123456789";
+        String strCopy = (String) Clone.cloneReflection(str);
+
+        assertEquals(str, strCopy);
+        assertNotSame(str, strCopy);
+
+        Board board = new Board();
+        board.put("a6", Piece.createBlackRook());
+        Board boardCopy = (Board) Clone.cloneReflection(board);
 
         assertEquals(board.print(), boardCopy.print());
         assertNotSame(board, boardCopy);
