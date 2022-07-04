e154bebd37898d37724417f5b91e0e95158c60a0
Pedro Santos <pedrojjsantos.cc@gmail.com>
2022-07-04 10:29:30 -0300

    Removing a method from the utility class StringUtil because it lost its use

--- Exercises/src/main/java/util/StringUtil.java
+++ Exercises/src/main/java/util/StringUtil.java
@@ -7,9 +7,6 @@ public class StringUtil {
     public static String appendNewLine(String str) {
         return str + NEWLINE;
     }
-    public static String join2Chars(int c1, int c2) {
-        return "" + (char) c1 + (char) c2;
-    }
 
     public static String truncate(String str, int i) {
         if (str.length() <= i)
--- Exercises/src/test/java/util/StringUtilTest.java
+++ Exercises/src/test/java/util/StringUtilTest.java
@@ -15,18 +15,6 @@ public class StringUtilTest {
     }
 
     @Test
-    public void testJoin2Chars() {
-        final char c1 = 'a';
-        final char c2 = 'b';
-
-        assertEquals("ab", StringUtil.join2Chars(c1, c2));
-        assertEquals("ba", StringUtil.join2Chars(c2, c1));
-
-        assertEquals("ab", StringUtil.join2Chars((int) c1, (int) c2));
-        assertEquals("ba", StringUtil.join2Chars((int) c2, (int) c1));
-    }
-
-    @Test
     public void truncate() {
         final String str = "abcdefghij";
 
