579d0ae8c3c81bcde61768f0f5a17a078b8efe58
Pedro Santos <pjjesus@sefaz.al.gov.br>
2022-07-15 15:04:46 -0300

    Removing repetition in the MyFileTest

--- Exercises/src/test/java/util/MyFileTest.java
+++ Exercises/src/test/java/util/MyFileTest.java
@@ -1,5 +1,6 @@
 package util;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.*;
@@ -9,7 +10,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class MyFileTest {
-    private final String fileName = "testMyFile.txt";
+    private final String filename = "testMyFile.txt";
     private final String content = _content();
     private final List<String> contentList = _contentList();
     MyFile file;
@@ -33,103 +34,84 @@ public class MyFileTest {
 
     @Before
     public void setUp() {
-        file = new MyFile(fileName);
+        file = new MyFile(filename);
+    }
+
+    @After
+    public void tearDown() {
+        File file = new File(filename);
+        if (file.exists())
+            assertTrue(file.delete());
     }
 
     @Test
     public void testWrite() throws IOException {
-        try {
-            file.write(content);
+        file.write(content);
 
-            String fileContent = readFile();
+        String fileContent = readFile();
+        assertEquals(content, fileContent);
+    }
+    private String readFile() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            StringBuilder buffer = new StringBuilder();
+            int ch;
 
-            assertEquals(content, fileContent);
-        }
-        finally {
-            deleteFile();
+            while ((ch = reader.read()) != -1)
+                buffer.append((char) ch);
+
+            return buffer.toString();
         }
     }
+
     @Test
     public void testWriteException() throws IOException{
         try {
             file.write("");
             file.write("");
             fail("Expected Exception");
-        } catch (FileAlreadyExistException success) {
-        }
-        finally {
-            deleteFile();
-        }
+        } catch (FileAlreadyExistException ignoredSuccess) {}
     }
 
     @Test
     public void testWriteLines() throws IOException {
-        try {
-            file.write(contentList);
+        file.write(contentList);
 
-            String fileContent = file.read();
-
-            assertEquals(content, fileContent);
-        }
-        finally {
-            deleteFile();
-        }
+        String fileContent = file.read();
+        assertEquals(content, fileContent);
     }
-
     @Test
     public void testWriteLinesException() throws IOException{
         try {
             file.write(new ArrayList<>());
             file.write(new ArrayList<>());
             fail("Expected Exception");
-        } catch (FileAlreadyExistException success) {
-        }
-        finally {
-            deleteFile();
-        }
+        } catch (FileAlreadyExistException ignoredSuccess) {}
     }
+
     @Test
     public void testOverwrite() throws IOException {
-        try {
-            file.write(content);
-            file.overwrite(content);
+        file.write("");
+        file.overwrite(content);
 
-            String fileContent = file.read();
-
-            assertEquals(content, fileContent);
-        }
-        finally {
-            deleteFile();
-        }
+        String fileContent = file.read();
+        assertEquals(content, fileContent);
     }
 
     @Test
     public void testOverwriteLines() throws IOException {
-        try {
-            file.write(contentList);
-            file.overwrite(contentList);
+        file.write(new ArrayList<>());
+        file.overwrite(contentList);
 
-            List<String> fileContent = file.readLines();
-
-            assertEquals(contentList, fileContent);
-        }
-        finally {
-            deleteFile();
-        }
+        List<String> fileContent = file.readLines();
+        assertEquals(contentList, fileContent);
     }
 
     @Test
     public void testRead() throws IOException {
-        try {
-            file.write(content);
+        file.write(content);
 
-            String fileContent = file.read();
-
-            assertEquals(content, fileContent);
-        }
-        finally {
-            deleteFile();
-        }
+        String fileContent = file.read();
+        assertEquals(content, fileContent);
     }
 
     @Test
@@ -138,24 +120,15 @@ public class MyFileTest {
             file.read();
             fail("Expected exception");
         }
-        catch (FileDoesntExistException success) {}
-        finally {
-            deleteFile();
-        }
+        catch (FileDoesntExistException ignoredSuccess) {}
     }
 
     @Test
     public void testReadLines() throws IOException {
-        try {
             file.write(contentList);
 
             List<String> fileContent = file.readLines();
-
             assertEquals(contentList, fileContent);
-        }
-        finally {
-            deleteFile();
-        }
     }
 
     @Test
@@ -164,27 +137,7 @@ public class MyFileTest {
             file.readLines();
             fail("Expected exception");
         }
-        catch (FileDoesntExistException success) {}
-        finally {
-            deleteFile();
-        }
+        catch (FileDoesntExistException ignoredSuccess) {}
     }
 
-
-    private String readFile() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-
-            StringBuilder buffer = new StringBuilder();
-
-            for (String line : reader.lines().toList()) {
-                buffer.append(line).append("%n".formatted());
-            }
-
-            return buffer.toString();
-        }
-    }
-    private void deleteFile() {
-        if (new File(fileName).exists())
-            assertTrue(file.delete());
-    }
 }
